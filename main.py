53d59f1ed982bf16fd404099aa7768ffb10d4584
hb818 <133333794+hb818@users.noreply.github.com>
2023-09-06 04:20:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,46 @@
-#Step 4
 
 import random
+import hangman_art
+import hangman_words
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
+word_list=hangman_words.word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+end_of_game = False
+lives = 6
 
-#Testing code
+print(hangman_art.logo)
+
 print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
-lives = 6
+
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    #Check guessed letter
-    not_right=True
+    if guess in display:
+        print("already guessed letter no deductions")
+
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-            not_right=False
-    if(not_right):
-        lives-=1
-        print(stages[lives])
 
-    #Join all the elements in the list and turn it into a String.
+    if guess not in chosen_word:
+        print("wrong choice bro you lose a life")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
     print(f"{' '.join(display)}")
 
-    #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win.")
-    elif(lives<=0):
-        end_of_game = True
-        print("Death")
 
+    print(hangman_art.stages[lives])
